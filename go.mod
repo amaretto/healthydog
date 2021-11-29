457b8e240788dd530b443ae72afa6ec9686c5ae6
amaretto <jabberwock26@gmail.com>
2021-11-30 00:54:07 +0900

    "WIP"

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/amaretto/watchdog
+
+go 1.16
+
+require github.com/aws/aws-sdk-go v1.42.14
--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
 )
 
 const (
@@ -46,10 +51,24 @@ type WeightSummary struct {
 	} `json:"weight"`
 }
 
+type RefreshResponce struct {
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    int    `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+	Scope        string `json:"scope"`
+	TokenType    string `json:"token_type"`
+	UserID       string `json:"user_id"`
+}
+
+type Secret struct {
+	AccessToken  string `json:"fitbit_token"`
+	RefreshToken string `json:"fitbit_refresh_token"`
+}
+
 func main() {
+
+	token, _ := getToken("fitbit", "ap-northeast-1")
 	date := "2021-11-21.json"
-	token := os.Getenv("FITBIT_TOKEN")
-	//refreshToken := os.Getenv("FITBIT_REFRESH_TOKEN")
 
 	// activity
 	apiEndPoint := baseURL + activityEndpoint + date
@@ -71,41 +90,100 @@ func main() {
 	fmt.Println("Distance ", activitySummary.Summary.Distances[0].Distance)
 	fmt.Println("Steps ", activitySummary.Summary.Steps)
 
-	// sleep
-	apiEndPoint = baseURL + sleepEndpoint + date
-	req, _ = http.NewRequest("GET", apiEndPoint, nil)
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
-	resp, err = http.DefaultClient.Do(req)
+	fmt.Println(resp.StatusCode)
+
+	//// sleep
+	//apiEndPoint = baseURL + sleepEndpoint + date
+	//req, _ = http.NewRequest("GET", apiEndPoint, nil)
+	//req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	//resp, err = http.DefaultClient.Do(req)
+	//if err != nil {
+	//	os.Exit(1)
+	//}
+	//defer resp.Body.Close()
+
+	//byteArray, _ = ioutil.ReadAll(resp.Body)
+
+	//var sleepSummary SleepSummary
+	//if err := json.Unmarshal(byteArray, &sleepSummary); err != nil {
+	//	os.Exit(1)
+	//}
+	//fmt.Println("Summary ", sleepSummary.Summary)
+	//fmt.Println("TotalMinutesAsleep ", sleepSummary.Summary.TotalMinutesAsleep)
+	//fmt.Println("TotalTimeInBed ", sleepSummary.Summary.TotalTimeInBed)
+
+	//// weight
+	//apiEndPoint = baseURL + weightEndpoint + date
+	//req, _ = http.NewRequest("GET", apiEndPoint, nil)
+	//req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	//resp, err = http.DefaultClient.Do(req)
+	//if err != nil {
+	//	os.Exit(1)
+	//}
+	//defer resp.Body.Close()
+
+	//byteArray, _ = ioutil.ReadAll(resp.Body)
+	//var weightSummary WeightSummary
+	//if err := json.Unmarshal(byteArray, &weightSummary); err != nil {
+	//	os.Exit(1)
+	//}
+	//fmt.Println("Weight ", weightSummary.Weight[0].Weight)
+	//fmt.Println("BMI ", weightSummary.Weight[0].Bmi)
+
+}
+
+func getToken(secretName, region string) (token, refreshToken string) {
+
+	sess, err := session.NewSession()
 	if err != nil {
-		os.Exit(1)
+		fmt.Println(err.Error())
+		return
 	}
-	defer resp.Body.Close()
-
-	byteArray, _ = ioutil.ReadAll(resp.Body)
-
-	var sleepSummary SleepSummary
-	if err := json.Unmarshal(byteArray, &sleepSummary); err != nil {
-		os.Exit(1)
+	svc := secretsmanager.New(sess,
+		aws.NewConfig().WithRegion(region))
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(secretName),
+		VersionStage: aws.String("AWSCURRENT"),
 	}
-	fmt.Println("Summary ", sleepSummary.Summary)
-	fmt.Println("TotalMinutesAsleep ", sleepSummary.Summary.TotalMinutesAsleep)
-	fmt.Println("TotalTimeInBed ", sleepSummary.Summary.TotalTimeInBed)
 
-	// weight
-	apiEndPoint = baseURL + weightEndpoint + date
-	req, _ = http.NewRequest("GET", apiEndPoint, nil)
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
-	resp, err = http.DefaultClient.Do(req)
+	result, err := svc.GetSecretValue(input)
 	if err != nil {
-		os.Exit(1)
-	}
-	defer resp.Body.Close()
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case secretsmanager.ErrCodeDecryptionFailure:
+				// Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+				fmt.Println(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
 
-	byteArray, _ = ioutil.ReadAll(resp.Body)
-	var weightSummary WeightSummary
-	if err := json.Unmarshal(byteArray, &weightSummary); err != nil {
+			case secretsmanager.ErrCodeInternalServiceError:
+				// An error occurred on the server side.
+				fmt.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
+
+			case secretsmanager.ErrCodeInvalidParameterException:
+				// You provided an invalid value for a parameter.
+				fmt.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
+
+			case secretsmanager.ErrCodeInvalidRequestException:
+				// You provided a parameter value that is not valid for the current state of the resource.
+				fmt.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
+
+			case secretsmanager.ErrCodeResourceNotFoundException:
+				// We can't find the resource that you asked for.
+				fmt.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	var secretString string
+	secretString = *result.SecretString
+	var secret Secret
+	if err := json.Unmarshal([]byte(secretString), &secret); err != nil {
 		os.Exit(1)
 	}
-	fmt.Println("Weight ", weightSummary.Weight[0].Weight)
-	fmt.Println("BMI ", weightSummary.Weight[0].Bmi)
+
+	return secret.AccessToken, secret.RefreshToken
 }
