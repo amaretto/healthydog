157a107f86db992d7ed9ce6fe2289239e5ae35b8
amaretto <jabberwock26@gmail.com>
2021-12-10 00:38:44 +0900

    separate fitbit.go from main.go add dd send function

---
+++ dd.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	datadog "github.com/DataDog/datadog-api-client-go/api/v1/datadog"
+)
+
+func send() {
+	body := datadog.MetricsPayload{
+		Series: []datadog.Series{
+			datadog.Series{
+				Metric: "weight",
+				Type:   datadog.PtrString("gauge"),
+				Points: [][]*float64{
+					{
+						datadog.PtrFloat64(float64(time.Now().Unix())),
+						datadog.PtrFloat64(81.4),
+					},
+				},
+				Tags: &[]string{
+					"test:ExampleSubmitmetricsreturnsPayloadacceptedresponse",
+				},
+			},
+		},
+	}
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	resp, r, err := apiClient.MetricsApi.SubmitMetrics(ctx, body, *datadog.NewSubmitMetricsOptionalParameters())
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `MetricsApi.SubmitMetrics`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `MetricsApi.SubmitMetrics`:\n%s\n", responseContent)
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/amaretto/watchdog
 
 go 1.16
 
-require github.com/aws/aws-sdk-go v1.42.14
+require (
+	github.com/DataDog/datadog-api-client-go v1.6.0
+	github.com/aws/aws-sdk-go v1.42.14
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	golang.org/x/net v0.0.0-20210716203947-853a461950ff // indirect
+)
